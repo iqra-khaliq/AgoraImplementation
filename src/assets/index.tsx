2f97afc8266d7f13bca6d71617ebe2d1c6c5e68f
iqra-khaliq <iqra.khaliq@viralsquare.org>
2025-05-22 16:12:27 +0500

    added assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,16 @@
+export const appIcons = {
+  imagePlaceholder: require('../assets/icons/profilePlaceholder.png'),
+  userPlaceholder: require('../assets/icons/placeholder.jpg'),
+  speakerOn: require('../assets/icons/speakerOn.png'),
+  speakerOff: require('../assets/icons/speakerOff.png'),
+  mute: require('../assets/icons/mute.png'),
+  muted: require('../assets/icons/muted.png'),
+  endCall: require('../assets/icons/endCall.png'),
+  flipCamera: require('../assets/icons/flip.png'),
+  showVideo: require('../assets/icons/video.png'),
+  hideVideo: require('../assets/icons/hideVideo.png'),
+};
+
+export const appImages = {
+  userPlaceholder: require('../assets/images/placeholder.jpg'),
+};
